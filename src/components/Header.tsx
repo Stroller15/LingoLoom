be7eb3c1b6058b7d24cafb7264bb1f1b6c46e9ea
Stroller15 <shubhamverma6351@gmail.com>
2023-10-30 12:11:50 +0530

    feat: :sparkles: add logo
    added logo of app

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Logo from "./Logo"
+
+const Header = () => {
+  return (
+    <header>
+        <nav>
+        {/* logo */}
+          <Logo/>
+        </nav>
+    </header>
+  )
+}
+
+
+export default Header;
+
+
+
+
---
+++ src/components/Logo.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import logo from "@logos/logo.png";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
+
+
+const Logo = () => {
+  return (
+    <Link href='/' prefetch={false} className="overflow-hidden">
+        <div className="flex items-center w-40 h-14">
+          <AspectRatio 
+          ratio={16/9}
+          className="flex items-center justify-center"
+          >
+            <Image
+            priority
+            src={logo}
+            alt="logo"
+            // className="rounded-full dark:filter dark:invert"
+            />
+          </AspectRatio>
+        </div>
+    </Link>
+  )
+}
+
+export default Logo

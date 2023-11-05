b6a113856df26fc3aae98fdcdf3564933853dd61
Stroller15 <shubhamverma6351@gmail.com>
2023-11-05 23:30:45 +0530

    feat: :lock: add authentication function

--- auth.ts
+++ auth.ts
@@ -6,5 +6,8 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
-    ]
+    ],
+    session: {
+        strategy: 'jwt',
+    }
 } satisfies NextAuthOptions;
--- src/components/CheckOutButton.tsx
+++ src/components/CheckOutButton.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client"
+
+import { useSession } from "next-auth/react";
 
 const CheckOutButton = () => {
-  return (
-    <div>CheckOutButton</div>
-  )
-}
+  const {data: session} = useSession();
 
-export default CheckOutButton
+  const createCheckOutSession = async () => {
+    if(!session) return;
+    // push document into firestore db
+
+    // stripe extension on firebase will create a checkout session
+
+    // redirect user to checkout page
+  }
+
+
+
+  return (
+    <div className="flex flex-col space-y-2">
+{/* If user is subscribed then show me the user is subscribed */}
+   
+    <button onClick={() => createCheckOutSession()} className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80">
+      Sign Up
+    </button>
+    </div>
+  );
+};
+
+export default CheckOutButton;
